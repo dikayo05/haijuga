36ebb3cbdfe15a2d0d6424bccc918b7c3579d8bf
Dika yonanda putra <yonandaputra05@gmail.com>
2025-01-19 16:18:12 +0700

    anu nambahin signin, signup, dll

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 // page
+import 'pages/signup_page.dart';
 import 'pages/home_page.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -26,7 +28,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlueAccent),
         useMaterial3: true,
       ),
-      home: HomePage(),
+      home: SignupPage(),
     );
   }
 }
---
+++ lib/models/user_model.dart
@@ -0,0 +1,15 @@
+class UserModel {
+  String id;
+  String name;
+  String email;
+  String password;
+
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.password,
+  });
+
+  // ...existing code...
+}
