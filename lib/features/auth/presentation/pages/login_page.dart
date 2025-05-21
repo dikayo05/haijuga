1d10860fed8187c2c180c3f80de66d03dfb7bdc5
Dika yonanda putra <yonandaputra05@gmail.com>
2025-05-21 21:15:37 +0700

    Update login page

--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -15,6 +15,8 @@ if user doesn't have an account yet, they can go to register page from here to c
 */
 
 import 'package:flutter/material.dart';
+import 'package:haijuga/features/auth/presentation/components/my_button.dart';
+import 'package:haijuga/features/auth/presentation/components/my_text_field.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -24,17 +26,78 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  // text controller
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
   // BUILD UI
   @override
   Widget build(BuildContext context) {
     // SCAFFOLD
     return Scaffold(
       // BODY
-      body: SafeArea(child: Column(
-        children: [
-          Icon(Icons.lock_open_rounded, size: 80, color: Theme.of(context).colorScheme.primary)
-        ],
-      )),
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // logo
+                Icon(
+                  Icons.lock_open_rounded,
+                  size: 80,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+
+                const SizedBox(height: 50),
+
+                // welcome back message
+                Text(
+                  "Welcome back, you've been missed!",
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontSize: 16,
+                  ),
+                ),
+
+                const SizedBox(height: 25),
+
+                // email text field
+                MyTextField(
+                  controller: emailController,
+                  hintText: "Email",
+                  obscureText: false,
+                ),
+
+                const SizedBox(height: 10),
+
+                // password text field
+                MyTextField(
+                  controller: passwordController,
+                  hintText: "Password",
+                  obscureText: true,
+                ),
+
+                const SizedBox(height: 25),
+
+                // login button
+                MyButton(onTap: () {}, text: "Login"),
+
+                const SizedBox(height: 50),
+
+                // not a member? register now button
+                Text(
+                  "don't have any account? Register here",
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
